6ce0634fcdb132a0f4b66ca69b79052883f0f2d3
Adler <nkj20932@hotmail.com>
2017-08-20 20:42:44 +1000

    add controllers

---
+++ app/controllers/api/v1/blog/posts_controller.rb
@@ -0,0 +1,5 @@
+class Api::V1::Blog::PostsController < Api::V1::ApiController
+  def index
+    
+  end
+end
---
+++ app/controllers/api/v1/forum/posts_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::Forum::PostsController < Api::V1::ApiController
+  def index
+    
+  end
+end
+
