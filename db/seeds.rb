ff36486f1de55826002b8ac068d718e494c4b519
Adler <nkj20932@hotmail.com>
2017-08-21 23:56:34 +1000

    add seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+Post.delete_all
+Comment.delete_all
+User.delete_all
+
+u1 = User.create(
+  email: Faker::Internet.email,
+  password: "12345678"
+)
+
+u2 = User.create(
+  email: Faker::Internet.email,
+  password: "12345678"
+)
+
+3.times do
+  post = Post.create(
+    {
+      title: Faker::Lorem.sentence(2),
+      content: Faker::Lorem.paragraph,
+      user: [u1, u2].sample
+    }
+  )
+  2.times do
+    post.comments.create(
+      {
+        content: Faker::Lorem.paragraph,
+        user: [u1, u2].sample
+      }
+    )
+  end
+end
