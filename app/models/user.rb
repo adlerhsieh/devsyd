c0f6529044b5380d6643b553d3161b1e432ba378
Adler <nkj20932@hotmail.com>
2017-08-23 22:41:29 +1000

    update forum/post/index view

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,33 @@ class User < ApplicationRecord
   has_many :comments
 
   def fullname
-    "#{firstname} #{lastname}"
+    if cjk?(firstname) || cjk?(lastname)
+      "#{lastname}#{firstname}"
+    else
+      "#{firstname} #{lastname}"
+    end
+  end
+
+  def cjk?(str)
+    list_of_chars = str.unpack("U*")
+    list_of_chars.each do |char|
+      #main blocks
+      if char >= 0x4E00 && char <= 0x9FFF
+        return true
+      end
+      #extended block A
+      if char >= 0x3400 && char <= 0x4DBF
+        return true
+      end
+      #extended block B
+      if char >= 0x20000 && char <= 0x2A6DF
+        return true
+      end
+      #extended block C
+      if char >= 0x2A700 && char <= 0x2B73F
+        return true
+      end
+    end
+    return false
   end
 end
