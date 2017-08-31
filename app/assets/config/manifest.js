f0c9ec019829d6868143d564b9f337f115913d5d
Adler Hsieh <adler@oneflare.com>
2017-08-31 12:25:18 +1000

    separate JS files in manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,7 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+
+//= link_directory ../javascripts/components .js
+
+//= link_directory ../javascripts/forum/posts .js
